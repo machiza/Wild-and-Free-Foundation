6b3579e5cbf400d8aac6ff1eb1cbf695e1f52b04
machiza <machizaarmando@gmail.com>
2018-08-01 16:03:26 +0200

    login with google

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,23 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('views', './views');
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/login', (req, res) => {
     res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.send('Wild and Free Foundation');
+    res.render('login/index', { layout: false });
 });
 
 app.get('/', (req, res) => {
     res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    res.send('Wild and Free Foundation');
+    res.render('index', { layout: false });
 });
 
 exports.app = functions.https.onRequest(app);
